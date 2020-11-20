59d0f15ea55f43e98bb21109187752d39d34c7e7
Alex Solda <alexsoldaa@gmail.com>
2020-11-19 22:09:14 -0300

    Refatorando o código com array

--- assets/app.js
+++ assets/app.js
@@ -3,91 +3,87 @@ const operation = document.querySelectorAll('#operation');
 const equal = document.querySelector('#equal');
 const del = document.querySelector('#ac');
 const display = document.querySelector('#visor');
-let numeroDisplay = '';
 let digit;
-let parcial = 0;
-let total = 0;
-let op;
+let displayValue = '0';
+let clearDisplay = false;
+let opt = null;
+let values = [0, 0];
+let indice = 0;
+
+//ATUALIZA O VALOR DO DISPLAY SEMPRE QUE A PÁGINA RECARREGA OU UM VALOR É ALTERADO
+function updateDisplay(currentValue) {
+    display.innerHTML = currentValue;
+}
+updateDisplay(displayValue);
 
 //ADICIONA O CLICK PARA CADA NÚEMRO DA CALCULADORA
 number.forEach((e) => {
     e.addEventListener('click', el => {
         digit = el.target.value;
         mostrarDisplay(digit);
-        display.innerHTML = numeroDisplay;
+        updateDisplay(values[indice]);
     })
 });
 
 //MOSTRA NA TELA OS NÚMEROS DIGITADOS
 function mostrarDisplay(digito) {
-    if (digito === '.' && numeroDisplay.includes('.')) {
+    if (digito === '.' && displayValue.includes('.')) {
         return
     }
-    numeroDisplay += digito;
-    return numeroDisplay;
+    const clearScreen = displayValue === '0' || clearDisplay;
+    const currentValue = clearScreen ? '' : displayValue;
+    displayValue = currentValue + digito;
+
+    if (digito != '.') {
+        const newValue = parseFloat(displayValue)
+        values[indice] = newValue;
+    }
 };
+
+operation.forEach((e => {
+    e.addEventListener('click', e => {
+        opt = e.target.value;
+        if (indice == 0) {
+            displayValue = '0';
+            indice = 1;
+            clearScreen = true;
+        } else {
+            const currentOpt = opt;
+
+            if (currentOpt == '+') {
+                values[0] = (values[0] + values[1]);
+            } else if (currentOpt == '-') {
+                values[0] = (values[0] - values[1]);
+            } else if (currentOpt == '*') {
+                values[0] = (values[0] * values[1]);
+            } else if (currentOpt == '/') {
+                values[0] = (values[0] / values[1]);
+            } else {
+                equals = opt === '=';
+                updateDisplay(values[0]);
+            }
+            opt = equals ? null : opt;
+            indice = equals ? 0 : 1;
+            clearScreen = !equals;
+            displayValue = '0';
+            values[1] = 0
+        }
+
+
+        updateDisplay(values[0]);
+    })
+}))
 
 
 //ADICIONA A FUNÇÃO DE APAGAR TODOS OS DADOS DA CALCULADORA
 del.addEventListener('click', () => {
     numeroDisplay = '';
-    total = 0;
-    parcial = 0;
     display.innerHTML = '0';
 });
 
-//DEFINE QUAL A OPERAÇÃO QUE ESTÁ SENDO REQUSITADA
-operation.forEach(e => {
-    e.addEventListener('click', el => {
-        op = el.target.value;
-        parcial = parseFloat(numeroDisplay);
-        numeroDisplay = '';
-    });
-});
 
 
-//RETORNA O RESULTADO NA TELA
-equal.addEventListener('click', () => {
-    equalCalc();
-    if(!isFinite(total)) {
-        display.innerHTML = (total = 0);
-    }else if (Number.isInteger(total)) {
-        display.innerHTML = total;
-    } else display.innerHTML = total.toFixed(2);
-
-});
 
 
-//TRATA AS OPERAÇÕES REQUISITADAS E RETORNA O TOTAL
-function equalCalc() {
-
-    switch (op) {
-        case '+':
-            if (!parcial) {
-                total += parseFloat(numeroDisplay);
-            } else total += (parcial + parseFloat(numeroDisplay));
-            break;
-        case '-':
-            if (!parcial) {
-                total -= parseFloat(numeroDisplay);
-            } else total += (parcial - parseFloat(numeroDisplay));
-            break;
-        case '*':
-            if (!parcial) {
-                total *= parseFloat(numeroDisplay);
-            } else total += (parcial * parseFloat(numeroDisplay));
-            break;
-        case '/':
-            if (!parcial) {
-                total /= parseFloat(numeroDisplay);
-            } else total += (parcial / parseFloat(numeroDisplay));
-            break;
-        default: total = parcial
-        }
-
-    parcial = 0;
-    numeroDisplay = '';
-    return total;
-};
 
 
